73c7a5f0e4dd0b8acf86a07ab1cb092b51077a35
unknown <wes_lima@outlook.com>
2022-09-28 11:59:37 -0300

    Criando e Estilizando os Primeiros Componentes

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return(
+        <>
+
+            <h1 className="titulo">Home</h1>
+            <img src="https://github.com/Wessslima.png" alt="Foto de Perfil" className="img"/>
+
+        </>
+    );
+}
+
+export default Home;
