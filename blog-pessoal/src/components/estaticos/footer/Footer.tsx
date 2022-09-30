1cfbe989dd091dca02b040c9f8f8e616c0fcebf6
unknown <wes_lima@outlook.com>
2022-09-30 01:48:10 -0300

    Criado os Componentes Navbar, Home e Footer

---
+++ blog-pessoal/src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+import purple from '@material-ui/core/colors/purple';
+
+const roxin = purple[500];
+
+function Footer() {
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid alignItems="center" item xs={12}>
+          <Box style={{ backgroundColor: roxin, height: '120px' }}>
+            <Box
+              paddingTop={1}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Typography
+                variant="h5"
+                align="center"
+                gutterBottom
+                style={{ color: 'white' }}
+              >
+                Siga-me nas redes sociais{' '}
+              </Typography>
+            </Box>
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <a
+                href="https://www.instagram.com/oeste.prado/"
+                target="_blank"
+              >
+                <InstagramIcon style={{ fontSize: 60, color: 'white' }} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/wessslima/"
+                target="_blank"
+              >
+                <LinkedInIcon style={{ fontSize: 60, color: 'white' }} />
+              </a>
+            </Box>
+          </Box>
+          <Box style={{ backgroundColor: '#303F9F', height: '60px' }}>
+            <Box paddingTop={1}>
+              <Typography
+                variant="subtitle2"
+                align="center"
+                gutterBottom
+                style={{ color: 'white' }}
+              >
+                © 2022 Copyright
+              </Typography>
+            </Box>
+            <Box>
+              <a target="_blank" href="https://github.com/Wessslima">
+                <Typography
+                  variant="subtitle2"
+                  gutterBottom
+                  style={{ color: 'white' }}
+                  align="center"
+                >
+                 WEST
+                </Typography>
+              </a>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default Footer;
--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,15 +1,36 @@
-import React from "react";
-import "./Home.css";
+import { Button, Paper } from '@material-ui/core';
+import { Box } from '@mui/material';
+import './Home.css';
+
+// import purple from '@material-ui/core/colors/purple';
+
+// const roxin = purple[500]
 
 function Home() {
-    return(
-        <>
-
-            <h1 className="titulo">Home</h1>
-            <img src="https://github.com/Wessslima.png" alt="Foto de Perfil" className="img"/>
-
-        </>
-    );
+  return (
+    <>
+      <Paper>
+        <Box p={2}>
+          <Box display="flex" justifyContent="center">
+            <h1>Título</h1>
+          </Box>
+          <img
+            src="https://i.imgur.com/wUf1NUF.png"
+            alt=""
+            className="imagem"
+          />
+          <Box display="flex" justifyContent="center" p={2}>
+            <Button variant="contained" color="primary">
+              Texto 1
+            </Button>
+            <Button variant="contained" color="secondary">
+              Texto 2
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
+    </>
+  );
 }
 
 export default Home;
